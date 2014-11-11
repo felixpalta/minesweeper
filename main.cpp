c54a22904923f606d76f3a072a929a302446d640
Felix Palta <fpalta90@gmail.com>
2014-11-12 01:03:45 +0300

    Use namespace std in main.cpp. Change input loop in main(), it is now more logical. Also check for extra characters after numbers input.

--- main.cpp
+++ main.cpp
@@ -2,64 +2,74 @@
 #include <vector>
 #include <exception>
 #include <stdexcept>
-
+#include <limits>
 #include "Mine_field.h"
 
 void drawMineField(const Mine_field& M);
 
 const int MIN_SIDE_SIZE = 2;
 
+using namespace std;
+
 int main()
 try
 {
     int field_rows_size = 0, field_cols_size = 0;
 
-    do
-    {
-        std::cout << "\nInput field size, N of rows and N of columns, \nboth >= " << MIN_SIDE_SIZE << ": ";
-        std::cin >> field_rows_size >> field_cols_size;
-        if (!std::cin)
-        {
-            std::cin.clear();
-            std::cin.ignore();
-            continue;
-        }
-    }
-    while (field_rows_size < MIN_SIDE_SIZE && field_cols_size < MIN_SIDE_SIZE);
 
-    std::cout << "Your field has " << field_rows_size << " rows and " << field_cols_size << " columns." <<  std::endl;
+    while (!(cout << "\nInput field size, N of rows and N of columns, \nboth >= " << MIN_SIDE_SIZE << ": ") ||
+           !(cin >> field_rows_size >> field_cols_size) ||
+           (field_rows_size < MIN_SIDE_SIZE && field_cols_size < MIN_SIDE_SIZE))
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int c;
+    if ((c = cin.peek()) != '\n')
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    cout << "Your field has " << field_rows_size << " rows and " << field_cols_size << " columns." <<  endl;
 
     Mine_field m(field_rows_size, field_cols_size);
 
     int number_of_mines = 0;
 
-    do
-    {
-        std::cout << "\nInput number of mines, N < " << m.cells_number() << ": ";
-        std::cin >> number_of_mines;
-        if (!std::cin)
-        {
-            std::cin.clear();
-            std::cin.ignore();
-            continue;
-        }
-    } while (number_of_mines >= m.cells_number() || number_of_mines < 1);
+    while (!(cout << "\nInput number of mines, N < " << m.cells_number() << ": ") ||
+           !(cin >> number_of_mines) ||
+           (number_of_mines >= m.cells_number() || number_of_mines < 1))
 
-    std::cout << "Your number of mines is " << number_of_mines << std::endl;
+    {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    if ((c = cin.peek()) != '\n')
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    cout << "Your number of mines is " << number_of_mines << endl;
 
     int select_row = -1, select_col = -1;
 
-    do
+    while (!(cout << "\nInput row and column number of the cell you want to start with: ") ||
+           !(cin >> select_row >> select_col) ||
+           !m.is_within_field(select_row, select_col))
     {
-        std::cout << "\nInput row and column number of the cell you want to start with: ";
-        std::cin >> select_row >> select_col;
-        if (!std::cin)
-        {
-            std::cin.clear();
-            std::cin.ignore();
-            continue;
-        }
-    } while (!m.is_within_field(select_row, select_col));
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    if ((c = cin.peek()) != '\n')
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
 
     m.generate_mines(number_of_mines, select_row, select_col);
     m.generate_neighbor_numbers();
@@ -67,9 +77,9 @@ try
     drawMineField(m);
     return 0;
 }
-catch (std::exception& e)
+catch (exception& e)
 {
-    std::cerr << e.what() << std::endl;
+    cerr << e.what() << endl;
     return 1;
 }
 
@@ -79,9 +89,9 @@ void drawMineField(const Mine_field& M)
     {
         for (int j = 0; j < M.cols(); ++j)
         {
-            std::cout << M.get_cell(i, j).value << '\t';
+            cout << M.get_cell(i, j).value << '\t';
         }
-        std::cout << std::endl;
+        cout << endl;
     }
 
 }
